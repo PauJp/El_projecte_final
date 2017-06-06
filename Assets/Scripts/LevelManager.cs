f2c2286906e602637f6b8ca5c5ed1a5163eae499
Unknown <pau.111@hotmail.es>
2017-06-06 22:43:39 +0200

    RESPAWNS TO WAPOS

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject Spawnpoint;
+
+    private PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<PlayerController>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RespawnPlayer()
+    {
+        Debug.Log("PlayerRespawn");
+        player.transform.position = Spawnpoint.transform.position;
+    }
+}
