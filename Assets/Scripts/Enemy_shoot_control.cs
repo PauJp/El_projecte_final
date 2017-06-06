b863b314dd73e7d6d542d3f299a338da1e692841
Unknown <pau.111@hotmail.es>
2017-06-06 21:10:31 +0200

    Enemics
    Enemics a tope de flama to locos

---
+++ Assets/Scripts/Enemy_shoot_control.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_shoot_control : MonoBehaviour {
+
+    public float speed;
+    public PlayerController player;
+    private Rigidbody2D myRigidbody2d;
+
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<PlayerController>();
+
+        myRigidbody2d = GetComponent <Rigidbody2D>();
+
+        if(player.transform.position.x > transform.position.x)
+        {
+            speed = -speed;
+
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myRigidbody2d.velocity = -new Vector2(speed, myRigidbody2d.velocity.y);
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.name == "Player")
+        {
+            //Posar aqui matar el player
+        }
+    }
+}
